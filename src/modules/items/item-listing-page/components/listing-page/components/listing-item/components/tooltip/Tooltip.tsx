0aa5a0c4221b3793f9f0937bbe82c4e3c3bda38b
akosfi <fiakos1997@gmail.com>
2023-09-18 10:58:25 +0200

    [fix] Fix CSS bugs.

--- src/modules/items/item-listing-page/components/listing-page/components/listing-item/components/tooltip/Tooltip.tsx
+++ src/modules/items/item-listing-page/components/listing-page/components/listing-item/components/tooltip/Tooltip.tsx
@@ -1,5 +1,5 @@
 import { Item } from 'modules/items/domain/Item';
-import { FC, useMemo, useState } from 'react';
+import { FC, useEffect, useMemo, useRef, useState } from 'react';
 import classNames from 'classnames';
 
 import css from './Tooltip.module.scss';
@@ -11,6 +11,22 @@ type ToolTipProps = {
 
 const ToolTip: FC<ToolTipProps> = ({ item, className }) => {
     const [isOpen, setIsOpen] = useState(false);
+    const [isAlignmentInverted, setIsAlignmentInverted] = useState(false);
+
+    const overlayRef = useRef<HTMLDivElement>(null);
+
+    useEffect(() => {
+        if (!overlayRef.current || !isOpen) {
+            return;
+        }
+
+        const { width: overlayWidth, left: xPositionOfOverlay } =
+            overlayRef.current.getBoundingClientRect();
+
+        if (overlayWidth + xPositionOfOverlay > document.body.clientWidth) {
+            setIsAlignmentInverted(true);
+        }
+    }, [overlayRef, isOpen]);
 
     const tooltipIconSrc = useMemo(
         () =>
@@ -27,32 +43,37 @@ const ToolTip: FC<ToolTipProps> = ({ item, className }) => {
             onMouseOut={() => setIsOpen(false)}
         >
             <img src={tooltipIconSrc} alt="tooltip" />
-            {isOpen && (
-                <div className={css['overlay']}>
-                    <div className={css['image']}>
-                        <img src={item.imageSrc} alt="item image" />
-                    </div>
-                    <div className={css['details']}>
-                        <p className={css['title']}>Description</p>
-                        <p className={css['description']}>{item.description}</p>
-                        <p className={css['title']}>Key features</p>
-                        <div className={css['key-features']}>
-                            <p>Lines</p>
-                            <p>{item.lines} Lines</p>
-                            <p>Display</p>
-                            <p>{item.display}</p>
-                            <p>Bluetooth</p>
-                            <p>{item.bluetooth}</p>
-                            <p>Connection</p>
-                            <p>{item.connection}</p>
-                            <p>USB</p>
-                            <p>{item.usb}</p>
-                            <p>PoE</p>
-                            <p>{item.poE ? 'Yes' : 'No'}</p>
-                        </div>
+
+            <div
+                className={classNames(css['overlay'], {
+                    [css['overlay-visible']]: isOpen,
+                    [css['overlay-invert-alignment']]: isAlignmentInverted,
+                })}
+                ref={overlayRef}
+            >
+                <div className={css['image']}>
+                    <img src={item.imageSrc} alt="item image" />
+                </div>
+                <div className={css['details']}>
+                    <p className={css['title']}>Description</p>
+                    <p className={css['description']}>{item.description}</p>
+                    <p className={css['title']}>Key features</p>
+                    <div className={css['key-features']}>
+                        <p>Lines</p>
+                        <p>{item.lines} Lines</p>
+                        <p>Display</p>
+                        <p>{item.display}</p>
+                        <p>Bluetooth</p>
+                        <p>{item.bluetooth}</p>
+                        <p>Connection</p>
+                        <p>{item.connection}</p>
+                        <p>USB</p>
+                        <p>{item.usb}</p>
+                        <p>PoE</p>
+                        <p>{item.poE ? 'Yes' : 'No'}</p>
                     </div>
                 </div>
-            )}
+            </div>
         </div>
     );
 };
